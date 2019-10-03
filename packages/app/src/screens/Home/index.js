031a0721fdc52cb0f75c69e576e1b48fcffae363
Bob Wei <bob.wei@linecorp.com>
2019-10-03 18:35:02 +0800

    Be able to display some photos

--- packages/app/src/screens/Home/index.js
+++ packages/app/src/screens/Home/index.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, FlatList, Image} from 'react-native';
+import CameraRoll from '@react-native-community/cameraroll';
+import * as R from 'ramda';
 
-import styles from './styles';
+import styles, {getPhotoStyle} from './styles';
 
 const Comp = () => {
+  const [photos] = usePhotos();
+  const numColumns = 4;
   return (
     <>
-      <View style={styles.container} />
+      <View style={styles.container}>
+        <FlatList
+          data={photos}
+          numColumns={numColumns}
+          renderItem={({item}) => {
+            return (
+              <View style={styles.photoContainer}>
+                <Image style={getPhotoStyle(numColumns)} source={item.image} />
+              </View>
+            );
+          }}
+          keyExtractor={R.path(['image', 'uri'])}
+        />
+      </View>
     </>
   );
 };
@@ -18,3 +35,18 @@ Comp.navigationOptions = () => {
 };
 
 export default Comp;
+
+function usePhotos() {
+  const [photos, setPhotos] = useState([]);
+  useEffect(() => {
+    CameraRoll.getPhotos({first: 10})
+      .then(
+        R.pipe(
+          R.prop('edges'),
+          R.map(R.prop('node')),
+        ),
+      )
+      .then(data => setPhotos(data));
+  }, []);
+  return [photos, setPhotos];
+}
--- packages/app/src/screens/Home/styles.js
+++ packages/app/src/screens/Home/styles.js
@@ -1,10 +1,19 @@
-import {StyleSheet} from 'react-native';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {StyleSheet, Dimensions} from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  photoContainer: {
+    padding: 2,
+  },
+});
+
+const {width} = Dimensions.get('window');
+
+export const getPhotoStyle = numColumns => ({
+  width: width / numColumns,
+  height: width / numColumns,
 });
 
 export default styles;
