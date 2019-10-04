5fe96d187499538ef0ac44f1979ca851e8b00ed3
Bob Wei <bob.wei@linecorp.com>
2019-10-04 17:41:02 +0800

    Add transform

--- packages/app/src/screens/Home/usePhotos.js
+++ packages/app/src/screens/Home/usePhotos.js
@@ -2,24 +2,24 @@ import {useEffect, useState} from 'react';
 import CameraRoll from '@react-native-community/cameraroll';
 import * as R from 'ramda';
 
+const transform = R.pipe(
+  R.prop('edges'),
+  R.map(R.prop('node')),
+  R.map(
+    R.applySpec({
+      source: R.pipe(
+        R.prop('image'),
+        R.pick(['uri']),
+      ),
+    }),
+  ),
+);
+
 function usePhotos() {
   const [photos, setPhotos] = useState([]);
   useEffect(() => {
     CameraRoll.getPhotos({first: 10})
-      .then(
-        R.pipe(
-          R.prop('edges'),
-          R.map(R.prop('node')),
-          R.map(
-            R.applySpec({
-              source: R.pipe(
-                R.prop('image'),
-                R.pick(['uri']),
-              ),
-            }),
-          ),
-        ),
-      )
+      .then(transform)
       .then(data => setPhotos(data));
   }, []);
   return [photos, setPhotos];
